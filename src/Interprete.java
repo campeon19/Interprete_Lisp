bd4a051ccee3228ee29d7ed0a073986e52a278d6
Jose Hurtarte <JJHH121106@gmail.com>
2020-02-21 11:51:14 -0600

    Ingreso read data

--- src/Interprete.java
+++ src/Interprete.java
@@ -1,4 +1,44 @@
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Interprete {
+	private ArrayList<Integer> datosNumericos;
+	private int resultado;
+	
+	public Interprete() {
+		datosNumericos = new ArrayList<Integer>();
+		
+	}
+
+	private boolean isOperator(String operator) {
+		
+		if (operator.equals("*")|| operator.equals("/")|| operator.equals("-")|| operator.equals("+"))
+			return true;
+		else
+		return false;
+	}
+
+	private boolean isNumber(String number) {
+		
+		if (number.equals("0")||number.equals("1")||number.equals("2")||number.equals("3")||number.equals("4")||number.equals("5")||number.equals("6")||number.equals("7")||number.equals("8")||number.equals("9"))
+			return true;
+		else
+		return false;
+	}
+	
+	public int readData(String entry) {
+		String[] datosTotales = entry.replaceAll(" ", "").split("");
+		String operando = "";
+		for (int i = 0; i < datosTotales.length; i++) {
+			if(isOperator(datosTotales[i])) {
+				if(isNumber(datosTotales[i+1]) && isNumber(datosTotales[i+2])) {
+					resultado= Integer.parseInt(datosTotales[i+1]) + Integer.parseInt(datosTotales[i+2]);
+				}
+			}
+			
+		}  
+		
+		 return resultado;
+	}
 
 }
