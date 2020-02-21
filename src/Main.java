f1d28577e7ed9c710ac108406514761de1984415
Jose Hurtarte <JJHH121106@gmail.com>
2020-02-21 11:01:18 -0600

    isNumber and isOperator addition in Main

--- src/Main.java
+++ src/Main.java
@@ -8,11 +8,30 @@
  */
 public class Main {
 
+	public static boolean isOperator(String operator) {
+		
+		if (operator.equals("*")|| operator.equals("/")|| operator.equals("-")|| operator.equals("+"))
+			return true;
+		else
+		return false;
+	}
+
+	public static boolean isNumber(String number) {
+		
+		if (number.equals("0")||number.equals("1")||number.equals("2")||number.equals("3")||number.equals("4")||number.equals("5")||number.equals("6")||number.equals("7")||number.equals("8")||number.equals("9"))
+			return true;
+		else
+		return false;
+	}
+	
+	
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
 	}
 
